89b738fbfbae79af36e926c2755e317989992f17
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-08-21 01:37:31 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 
 var element=document.getElementById("main-text");
 element.innerHTML="New Text";
+
+var img=document.getElementById("madi");
+
+img.onclick = function()
+{
+    img.style.marginLeft='100px';
+}
