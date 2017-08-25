259d23ff55f6f00da54bed9ca39aa98ce18da9af
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-08-25 07:45:28 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,3 +48,27 @@ button.onclick = function(){
     request.send(null);
     
 };
+
+
+
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var Submit=document.getElementById("Submit_btn");
+Submit.onlick=function(){
+    //make a request to the server and send the name
+    
+    //capture a list of names and render it as a list
+    
+    
+    var names= ['name1','name2','name3','name4'];
+    
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>' + names[i] + '</li>';
+    }
+    
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+    
+};
+
