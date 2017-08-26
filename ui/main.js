e0d2f04ff44e9942ac79fb448a2160c5649baa60
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-08-25 23:06:12 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,7 +64,7 @@ Submit.onlick=function(){
     
     var list='';
     for(var i=0;i<names.length;i++){
-        list+='<li>' + names[i] + '</li>';
+        list+='<li>' + names[i] + '</li>';Console.log(list);
     }
     
     var ul=document.getElementById('namelist');
