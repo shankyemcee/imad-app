5c8b0f4030c8680fe7b2f8f455796eed16ee8c4d
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-08-26 23:36:36 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -88,7 +88,7 @@ submit.onclick =function(){
             //take some action
             if(request.status===200){
                 var names=request.responseText;
-                name=JSON.parse(names);
+                names=JSON.parse(names);
                 var list='';
                 for(var i=0;i<names.length;i++)
                 {
