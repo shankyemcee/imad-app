d77eb80996fdbccbe29699832137b8263b331368
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-09-02 08:21:42 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get('/articles/:articleName', function (req, res) {
 
 
 //select * from article where title='article-one'
-pool.query("SELECT * FROM dept WHERE title= "+ req.params.articleName  , function(err,result){
+pool.query("SELECT * FROM dept WHERE title='"+ req.params.articleName + "'", function(err,result){
     if(err){
         res.status(500).send(err.toString());
     }
