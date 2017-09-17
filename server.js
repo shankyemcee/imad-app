81cac64ce9259f4172596d36294479974b09c62f
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-09-17 02:24:33 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 
 var config={
     user:'shankyemcee',
@@ -163,6 +164,27 @@ pool.query("SELECT * FROM dept WHERE title=$1" ,[req.params.articleName], functi
 });
   
 });
+
+
+function hash(input,salt)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    
+    return hashed.toString('hex');
+}
+
+
+
+
+app.get('/hash/:input',function(req,res){
+    
+    var hashedString=hash(req.params.input,'this-is-some-random-value');
+    res.send(hashedString);
+});
+
+
+
+
 
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
