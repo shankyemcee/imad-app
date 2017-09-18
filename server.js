2311a16981d01193231c9af9ab60b969866a8b96
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-09-17 23:39:14 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 
 var config={
     user:'shankyemcee',
@@ -15,6 +16,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -184,6 +186,24 @@ app.get('/hash/:input',function(req,res){
 
 
 
+app.post('/create-user',function(req,res){
+    //username,password
+    //JSON
+    var username=req.body.username;
+    var password=req.body.password;
+    
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    
+    pool.query('INSERT INTO person (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send('User succesfully created:' + username);
+        }
+    });
+});
 
 
 app.get('/article-one', function (req, res) {
