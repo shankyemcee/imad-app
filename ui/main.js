b7dd3eb8d9aa7e6dd8c92470b1bfe6644afe2f4a
shankyemcee <30503832+shankyemcee@users.noreply.github.com>
2017-09-18 22:48:34 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -133,13 +133,13 @@ mooo.onclick = function() {
         }
     };
   
-    var username=document.getElementById('username'); 
-    var password=document.getElementById('password');
+    var username=document.getElementById('username').value; 
+    var password=document.getElementById('password').value;
     console.log(username);
     console.log(password);
     request.open('POST','http://shankyemcee.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({ username : 'shankar', password : 'password' }));
+    request.send(JSON.stringify({ username : username , password : password }));
 };
 
 
